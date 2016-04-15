6362be7bcf831e5f4a9fa559142825acb673c343
Jason Bell <jason@mastodonc.com>
2016-04-15 16:00:41 +0100

    Added json/clj-http libs to project. Created functions to pull data from the API and extract the entity ids.

--- project.clj
+++ project.clj
@@ -4,7 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-		 [org.clojure/tools.cli "0.3.3"]]
+		 [org.clojure/tools.cli "0.3.3"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/data.csv "0.1.3"]
+                 [clj-http "2.1.0"]]
   :main ^:skip-aot kixi.hecuba.weather.service
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/kixi/hecuba/weather/service.clj
+++ src/kixi/hecuba/weather/service.clj
@@ -1,24 +1,48 @@
 (ns kixi.hecuba.weather.service
   (:require [clojure.java.io :as io]
-            [clojure.tools.cli :refer [parse-opts]])
+            [clojure.tools.cli :refer [parse-opts]]
+            [clj-http.client :as client]
+            [clojure.data.json :as json]
+            [clojure.string :as str])
   (:gen-class))
 
 (def cli-options
   [["-c" "--config CONFIG" "Path to config edn file."
-    :default "config/settings.edn"]])
+    :default "config/settings.edn"]
+   ["-u" "--username USERNAME" "Your username."]
+   ["-p" "--password PASSWORD" "Your password."]])
 
 (defn load-config [filename]
   (with-open [r (io/reader filename)]
     (read (java.io.PushbackReader. r))))
 
-(defn find-entities [project-id api-endpoint entity-type]
-  (println project-id)
-  (println api-endpoint)
-  (println entity-type))
+(defn run-api-search [username password project-id api-endpoint entity-type max-entries-per-page]
+  (let [url-to-get (str api-endpoint
+                        "entities/?q=property_type:"
+                        entity-type "&page=0&size="
+                        max-entries-per-page
+                        "&sort_key=programme_name.lower_case_sort&sort_order=asc")]
+    (try (let [response-json (-> (:body (client/get
+                                         url-to-get
+                                         {:basic-auth [username password]
+                                          :headers {"X-Api-Version" "2"}
+                                          :content-type :json
+                                          :socket-timeout 20000
+                                          :conn-timeout 20000}))
+                                 (json/read-str))]
+           response-json)
+         (catch Exception e (println e)))))
 
+(defn get-entity-ids [username password project-id api-endpoint entity-type  max-entries-per-page]
+  (let [entities (-> (run-api-search username password project-id api-endpoint entity-type max-entries-per-page)
+                     (get-in ["entities"]))]
+    (mapv (fn [entity] (get entity "entity_id")) entities)))
+
+;; TODO - read hecuba username/password from the command line opt, do not place in edn file.
 (defn -main [& args]
   (let [config (-> (parse-opts args cli-options) :options :config)
         {:keys [project-id api-endpoint entity-type]} (load-config config)]
-    (find-entities project-id
-                   api-endpoint
-                   entity-type)))
+    (get-entity-ids
+     project-id
+     api-endpoint
+     entity-type)))
