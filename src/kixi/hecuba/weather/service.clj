5cff2c6a7ee8312767e09d720a354c2d6b16fc76
Jase Bell <jasebell@gmail.com>
2016-04-15 11:18:28 +0100

    Added CLI tools library. (#1)
    Added CLI tools library and EDN config file support.

--- src/kixi/hecuba/weather/service.clj
+++ src/kixi/hecuba/weather/service.clj
@@ -1,7 +1,24 @@
 (ns kixi.hecuba.weather.service
+  (:require [clojure.java.io :as io]
+            [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def cli-options
+  [["-c" "--config CONFIG" "Path to config edn file."
+    :default "config/settings.edn"]])
+
+(defn load-config [filename]
+  (with-open [r (io/reader filename)]
+    (read (java.io.PushbackReader. r))))
+
+(defn find-entities [project-id api-endpoint entity-type]
+  (println project-id)
+  (println api-endpoint)
+  (println entity-type))
+
+(defn -main [& args]
+  (let [config (-> (parse-opts args cli-options) :options :config)
+        {:keys [project-id api-endpoint entity-type]} (load-config config)]
+    (find-entities project-id
+                   api-endpoint
+                   entity-type)))
